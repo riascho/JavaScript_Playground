4aa65dbf12e529390ff5a2b25db358179ce90daf
surfineer <123465523+surfineer@users.noreply.github.com>
2024-01-30 16:07:43 +0100

    init project

---
+++ randomMessageGenerator/script.js
@@ -0,0 +1,5 @@
+const nouns = ["pasta","movie","music","species","cake","color","island","salt","umbrella","bathtub"];
+const adjectives = ["new","expensive", "rich","pink","futuristic","free","imported","ugly","spooky"];
+const verbs = ["create","invent","discover","reproduce","notice","complain about","wonder about", "think about"];
+
+console.log(Math.floor(Math.random()*nouns.length));
