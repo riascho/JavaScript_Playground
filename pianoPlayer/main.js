59c882c5994aa525c6383c4f12240367c86edc43
surfineer <123465523+surfineer@users.noreply.github.com>
2024-02-04 11:30:59 +0100

    rework after PR feedback

--- pianoPlayer/main.js
+++ pianoPlayer/main.js
@@ -9,40 +9,39 @@
 // The keys and notes variables store the piano keys
 // plucks out the corresponding DOM elements and stores them as such
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
-const notes = [];
-keys.forEach(function (key) {
-    notes.push(document.getElementById(key));
+const notes = keys.map(function (key) {
+    return document.getElementById(key);
 })
 
 // simple skeuomorphic design for when a key is played
-function keyPlay(event) {
+function applyKeyDownVisual(event) {
     event.target.style.backgroundColor = 'darkgrey';
     event.target.style.position = 'relative';
     event.target.style.top = '5px';
     event.target.style.left = '5px';
 }
-function keyReturn(event) {
+function applyKeyReleaseVisual(event) {
     event.target.style.backgroundColor = '';
     event.target.style.position = '';
     event.target.style.top = '';
     event.target.style.left = '';
 }
-function playNote(note) {
-    note.addEventListener('mousedown', keyPlay);
-    note.addEventListener('mouseup', keyReturn);
+function attachKeyPlayEventListeners(note) {
+    note.addEventListener('mousedown', applyKeyDownVisual);
+    note.addEventListener('mouseup', applyKeyReleaseVisual);
 }
 
 // loop that assigns the eventListeners to each key DOM element
-notes.forEach(playNote);
+notes.forEach(attachKeyPlayEventListeners);
 
 // These variables store the buttons that progress the user through the lyrics
-let nextOne = document.getElementById('first-next-line');
-let nextTwo = document.getElementById('second-next-line');
-let nextThree = document.getElementById('third-next-line');
-let startOver = document.getElementById('fourth-next-line');
+const nextOne = document.getElementById('first-next-line');
+const nextTwo = document.getElementById('second-next-line');
+const nextThree = document.getElementById('third-next-line');
+const startOver = document.getElementById('fourth-next-line');
 
 // This variable stores the '-END' lyric element
-let lastLyric = document.getElementById('column-optional');
+const lastLyric = document.getElementById('column-optional');
 
 // These statements are "hiding" all the progress buttons, but the first one
 nextTwo.hidden = true;
@@ -51,29 +50,27 @@ startOver.hidden = true;
 
 // anonymous event handler property and function for the first progress button
 nextOne.onclick = function () {
-    nextTwo.hidden = false;
-    nextOne.hidden = true;
     document.getElementById('letter-note-five').innerHTML = 'D';
     document.getElementById('letter-note-six').innerHTML = 'C';
+    nextTwo.hidden = false;
+    nextOne.hidden = true;
 }
 
 // anonymous event handler property and function for the second progress button
 nextTwo.onclick = function () {
-    nextThree.hidden = false;
-    nextTwo.hidden = true;
     document.getElementById('word-five').innerHTML = 'DEAR';
     document.getElementById('word-six').innerHTML = 'FRI-';
-    lastLyric.style.display = 'inline-block';
     document.getElementById('letter-note-three').innerHTML = 'G';
     document.getElementById('letter-note-four').innerHTML = 'E';
     document.getElementById('letter-note-five').innerHTML = 'C';
     document.getElementById('letter-note-six').innerHTML = 'B';
+    nextThree.hidden = false;
+    nextTwo.hidden = true;
+    lastLyric.style.display = 'inline-block';
 }
 
 // anonymous event handler property and function for the third progress button
 nextThree.onclick = function () {
-    startOver.hidden = false;
-    nextThree.hidden = true;
     document.getElementById('word-one').innerHTML = 'HAP-';
     document.getElementById('word-two').innerHTML = 'PY';
     document.getElementById('word-three').innerHTML = 'BIRTH';
@@ -86,13 +83,13 @@ nextThree.onclick = function () {
     document.getElementById('letter-note-four').innerHTML = 'C';
     document.getElementById('letter-note-five').innerHTML = 'D';
     document.getElementById('letter-note-six').innerHTML = 'C';
+    startOver.hidden = false;
+    nextThree.hidden = true;
     lastLyric.style.display = 'none';
 }
 
 // This is the event handler property and function for the startOver button
 startOver.onclick = function () {
-    nextOne.hidden = false;
-    startOver.hidden = true;
     document.getElementById('word-one').innerHTML = 'HAP-';
     document.getElementById('letter-note-one').innerHTML = 'G';
     document.getElementById('word-two').innerHTML = 'PY';
@@ -105,4 +102,6 @@ startOver.onclick = function () {
     document.getElementById('letter-note-five').innerHTML = 'C';
     document.getElementById('word-six').innerHTML = 'YOU!';
     document.getElementById('letter-note-six').innerHTML = 'B';
+    nextOne.hidden = false;
+    startOver.hidden = true;
 }
