d3caa60f28452e6868077457bee41cb056218cce
surfineer <123465523+surfineer@users.noreply.github.com>
2024-02-13 22:02:44 +0100

    add styling and js functionality

--- typewriter/script.js
+++ typewriter/script.js
@@ -1,15 +1,35 @@
 const techStack = ['Python', 'Flask', 'Javascript', 'HTML', 'CSS'];
 const techStackSpan = document.getElementById('typewriter');
-const intervalMilliSeconds = 1500;
+const pauseBetweenWords = 1500;
+const typingInterval = 150;
 let indexTechStack = 0;
+let indexWord = 0;
 
-function updateTechStack() {
-    techStackSpan.innerText = techStack[indexTechStack];
-    if (indexTechStack < techStack.length - 1) {
-        indexTechStack++;
+function typeTechStack() {
+    if (indexWord < techStack[indexTechStack].length) {
+        // Add next character
+        techStackSpan.textContent += techStack[indexTechStack].charAt(indexWord);
+        indexWord++;
+        setTimeout(typeTechStack, typingInterval);
     } else {
-        indexTechStack = 0;
+        // Pause after typing a word
+        setTimeout(() => {
+            techStackSpan.textContent = ''; // Clear the span
+            indexWord = 0; // Reset character index
+            indexTechStack = (indexTechStack + 1) % techStack.length; // when last index modulo array length is zero = reset index
+            typeTechStack(); // Start typing the next word
+        }, pauseBetweenWords);
     }
 }
+document.addEventListener('DOMContentLoaded', typeTechStack);
 
-document.addEventListener('DOMContentLoaded', setInterval(updateTechStack, intervalMilliSeconds));
+// my original cripply code
+// function updateTechStack() {
+//     techStackSpan.innerText = techStack[indexTechStack];
+//     if (indexTechStack < techStack.length - 1) {
+//         indexTechStack++;
+//     } else {
+//         indexTechStack = 0;
+//     }
+// }
+// document.addEventListener('DOMContentLoaded', setInterval(updateTechStack, pauseBetweenWords));
