fe8e6801c60dd3b13041374fdbe667966f419a98
Maria Scholz <123465523+mariaremote@users.noreply.github.com>
2024-02-25 12:11:33 +0000

    progress log

--- bookFinder/script.js
+++ bookFinder/script.js
@@ -1,34 +1,33 @@
-// Click handler for search button
 const captureSearchValue = () => {
     const searchValue = document.getElementById('search-bar').value;
     return searchValue;
 };
-// const searchButton = document.getElementById('search-btn');
-// searchButton.addEventListener('click', () => {
-//     searchValue ? console.log(searchValue) : console.log('empty');
-// });
 
 // Filter books based on search input
 /*
-The filterBooks() function takes in a search string and a list of books as parameters and returns all of the books that contain an exact match of the search input as an array of objects. Objects in this array should be formatted as books with title, author, and tags properties, similar to the original books array. It should use the flattenObjectValuesIntoArray() function to search all fields within a book object easily.
+The filterBooks() function takes in a search string and a list of books as parameters 
+and returns all of the books that contain an exact match of the search input as an array of objects. 
+Objects in this array should be formatted as books with title, author, and tags properties, 
+similar to the original books array. It should use the flattenObjectValuesIntoArray() function 
+to search all fields within a book object easily.
 */
 const filterBooks = (searchString, library) => {
-    // return resultArray; (array of objects!)
+    const listOfBooks = flattenObjectValuesIntoArray(library);
+    // const results = listOfBooks.filter((element, index) => element.includes('fiction').map((_, index) => index));
+    const matchingBooksIndexes = listOfBooks
+        .map((book, index) => {
+            if (book.includes(searchString)) {
+                return index;
+            }
+        })
+        .filter((element) => element !== undefined);
+    // return array of objects! -> get object by mapping books[index] into new array
+    return matchingBooksIndexes;
 };
-const listOfBooks = flattenObjectValuesIntoArray(library);
 
-// returns array of arrays of search results (WORKS!)
-flattenObjectValuesIntoArray(books).filter((element, index, array) => {
-    return element.includes('fiction');
-});
+// TESTING:
+// filterBooks('fiction',books);
 
-const matchingBooksIndexes = listOfBooks.filter((bookArray, index) => {
-    bookArray.includes(searchString);
-    return index; // returns list of indexes?
-});
-const resultArray = library.map((element, index, array) => {
-    matchingBooksIndexes.includes(array[index]);
-});
 // Empty the book list container, iterate over list of filtered books, return list of books formatted as HTML using the function in `helper.js`
 // The structureBooksAsHtml() function takes a list of books as a parameter, iterates over the list, formats them as HTML using the structureBookAsHtml() helper function, and returns an array of formatted book elements.
 const structureBooksAsHtml = () => {};
