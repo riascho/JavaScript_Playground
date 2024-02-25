ecc06cb235f7ac9646b7778906e54ca84a471133
Maria Scholz <123465523+mariaremote@users.noreply.github.com>
2024-02-25 16:35:16 +0000

    finished documentation

--- bookFinder/README.md
+++ bookFinder/README.md
@@ -2,6 +2,8 @@
 
 ## Book Finder Website
 
+![screenshot website display](image.png)
+
 ### End Result / Goal
 
 This site allows the user to input a genre, title, or author into the search bar, and get back a list of books that match that criteria.
@@ -13,4 +15,10 @@ This site allows the user to input a genre, title, or author into the search bar
 
 ### Task
 
-The task is to finish building the application by completing the function definitions for the `captureSearchValue()`, `filterBooks()`, `structureBooksAsHtml()`, and `searchBtnClickHandler()` functions. Edits should only be made to these four function definitions within `script.js`.
+The task was to finish building the application by completing the function definitions for the `captureSearchValue()`, `filterBooks()`, `structureBooksAsHtml()`, and `searchBtnClickHandler()` functions. Edits should only be made to these four function definitions within `script.js`.
+
+### Limitations (Potential Upgrades)
+
+1. Currently the search mechanism relies on an exact match. The next improvement should make the search more robust by using **case-insensitivity** and **single words** in the results array (e.g. typing in `"orwell"` will match `"George Orwell"`)
+
+2. There is no way to view the entire book library. One possibility to improve this could be to implement a 2nd button that will just render the entire book library or this could be achieved by clicking the `search` button with no value entered in the search bar.
--- bookFinder/script.js
+++ bookFinder/script.js
@@ -3,51 +3,23 @@ const captureSearchValue = () => {
     return searchValue;
 };
 
-// Filter books based on search input
-/*
-The filterBooks() function takes in a search string and a list of books as parameters 
-and returns all of the books that contain an exact match of the search input as an array of objects. 
-Objects in this array should be formatted as books with title, author, and tags properties, 
-similar to the original books array. It should use the flattenObjectValuesIntoArray() function 
-to search all fields within a book object easily.
-*/
 const filterBooks = (searchString, library) => {
     const listOfBooks = flattenObjectValuesIntoArray(library);
-    const matchingBooksIndexes = listOfBooks
+    const matchingBooksIndices = listOfBooks
         .map((book, index) => {
             if (book.includes(searchString)) {
                 return index;
             }
         })
         .filter((element) => element !== undefined);
-    return matchingBooksIndexes.map((item) => library[item]);
+    return matchingBooksIndices.map((item) => library[item]);
 };
 
-// TESTING:
-// filterBooks('fiction',books);
-
-// Empty the book list container, iterate over list of filtered books, return list of books formatted as HTML using the function in `helper.js`
-// The structureBooksAsHtml() function takes a list of books as a parameter, iterates over the list, formats them as HTML using the structureBookAsHtml() helper function, and returns an array of formatted book elements.
 const structureBooksAsHtml = (bookObjects) => bookObjects.map((bookObject) => structureBookAsHtml(bookObject));
 
-// Handler triggered when a user clickers the "Search" button.
-// Chains previously defined functions together to filter books
-// based on the search value, formats the books as HTML and
-// renders them to the DOM
-// The searchBtnClickHandler() function is triggered when a
-// user clicks the search button.
-// It takes in a list of books as a parameter and integrates
-// the individual functions that make up the app to render
-// a list of books to the DOM that matches the search input
-// when the search button is clicked.
 const searchBtnClickHandler = (listOfBookObjects) => {
     renderBooksToDom(structureBooksAsHtml(filterBooks(captureSearchValue(), listOfBookObjects)));
 };
 
-// Grab search button from the DOM
 const searchBtn = document.getElementById('search-btn');
-
-// Attach an event listener to the search button
-searchBtn.addEventListener('click', () => {
-    searchBtnClickHandler(books);
-});
+searchBtn.addEventListener('click', () => searchBtnClickHandler(books));
