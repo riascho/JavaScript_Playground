b5b8bde55e901a1a69833a109291ebc0c5219016
surfineer <123465523+surfineer@users.noreply.github.com>
2024-02-29 19:37:32 +0100

    enhanced CSS transition, logo shift on scroll

--- scrollNavbar/script.js
+++ scrollNavbar/script.js
@@ -1,6 +1,8 @@
 
+
 window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
+    const logo = document.getElementById('logo');
     header.classList.toggle('sticky',window.scrollY > 0);
-    console.log(window.scrollY);
+    logo.classList.toggle('shift',window.scrollY > 0);
 });
