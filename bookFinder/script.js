51506e9fc4a5820101746938139f181e14c10140
surfineer <123465523+surfineer@users.noreply.github.com>
2024-02-29 08:54:23 +0100

    reworked filtering books functionality

--- bookFinder/script.js
+++ bookFinder/script.js
@@ -3,16 +3,31 @@ const captureSearchValue = () => {
     return searchValue;
 };
 
+// 1st attempt using map and filter
+// const filterBooks = (searchString, library) => {
+//     const listOfBooks = flattenObjectValuesIntoArray(library);
+//     const matchingBooksIndices = listOfBooks
+//         .map((book, index) => {
+//             if (book.includes(searchString)) {
+//                 return index;
+//             }
+//         })
+//         .filter((element) => element !== undefined);
+//     return matchingBooksIndices.map((item) => library[item]);
+// };
+
+// better solution using reduce
 const filterBooks = (searchString, library) => {
     const listOfBooks = flattenObjectValuesIntoArray(library);
-    const matchingBooksIndices = listOfBooks
-        .map((book, index) => {
+    const matchingBooks = listOfBooks
+        .reduce((matchedBooks, book, index) => {
             if (book.includes(searchString)) {
-                return index;
+                matchedBooks.push(library[index]);
             }
-        })
-        .filter((element) => element !== undefined);
-    return matchingBooksIndices.map((item) => library[item]);
+
+            return matchedBooks
+        },[])
+    return matchingBooks;
 };
 
 const structureBooksAsHtml = (bookObjects) => bookObjects.map((bookObject) => structureBookAsHtml(bookObject));
